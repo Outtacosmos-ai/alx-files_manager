7855805a45abb8ea811a94e4caae978f26ab5a6b
Outtacosmos-ai <messghir4@gmail.com>
2024-12-08 00:26:43 +0100

    Authenticate a user

---
+++ controllers/AuthController.js
@@ -0,0 +1,42 @@
+import { v4 as uuidv4 } from 'uuid';
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AuthController {
+  static async getConnect(req, res) {
+    const authHeader = req.header('Authorization');
+    if (!authHeader || !authHeader.startsWith('Basic ')) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const credentials = Buffer.from(authHeader.slice(6), 'base64').toString().split(':');
+    if (credentials.length !== 2) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const [email, password] = credentials;
+    const user = await dbClient.client.db().collection('users').findOne({ email, password: sha1(password) });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const token = uuidv4();
+    await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
+
+    return res.status(200).json({ token });
+  }
+
+  static async getDisconnect(req, res) {
+    const token = req.header('X-Token');
+    if (!token) return res.status(401).json({ error: 'Unauthorized' });
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) return res.status(401).json({ error: 'Unauthorized' });
+
+    await redisClient.del(`auth_${token}`);
+    return res.status(204).send();
+  }
+}
+
+export default AuthController;
