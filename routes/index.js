800378eb550c754e41d2ec5f4b391d30667cff37
Outtacosmos-ai <messghir4@gmail.com>
2024-12-09 19:38:47 +0100

    3

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,9 @@
-import express from 'express';
-import AppController from '../controllers/AppController';
+// routes/index.js
+import { Router } from 'express';
 import UsersController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
 
-const router = express.Router();
+const router = Router();
 
-// App status and stats routes
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-
-// User routes
 router.post('/users', UsersController.postNew);
-router.get('/users/me', UsersController.getMe);
-
-// Authentication routes
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-
-// Files routes
-router.post('/files', FilesController.postUpload);
 
 export default router;
